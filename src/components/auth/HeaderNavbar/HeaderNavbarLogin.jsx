da9c18ecb3ad2bbe80e4927105d64c8233a9816c
Byron-Poveda <119381029+Byron-Poveda@users.noreply.github.com>
2023-12-07 15:00:37 -0500

    implementacion de hook useDeviceDetected

--- src/components/auth/HeaderNavbar/HeaderNavbarLogin.jsx
+++ src/components/auth/HeaderNavbar/HeaderNavbarLogin.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import logo_desktop from '../../../assets/images/logos/logo-desktop.png'
+import logo_tablet from '../../../assets/images/logos/logo-tablet.png'
+import logo_mobile from '../../../assets/images/logos/logo-mobile.png'
 import './_HeaderNavbarLogin.scss'
 import NavLinkHeader from 'components/NavLinkHeader/NavLinkHeader'
 import { useDeviceDetect } from 'hooks/deviceDetect/useDeviceDetect'
@@ -7,15 +9,10 @@ import { useDeviceDetect } from 'hooks/deviceDetect/useDeviceDetect'
 function HeaderNavbarLogin() {
   const { isMobile, isTablet, isDesktop } = useDeviceDetect()
 
-  useEffect(()=>{
-    console.log("isMobile:", isMobile)
-    console.log("isTablet:", isTablet)
-    console.log("isDesktop:", isDesktop)
-  },[isMobile, isTablet, isDesktop])
   return (
     <div className='HeaderNavbarLogin'>
       <div className='HeaderNavbarLogin__wrapper'>
-        <img src={logo_desktop} alt="logo desktop" />
+        <img src={isDesktop ? logo_desktop : isTablet ? logo_tablet : logo_mobile} alt="logo desktop" />
         <ul className='HeaderNavbarLogin__list'>
           <li className='pointer'>
             <NavLinkHeader
