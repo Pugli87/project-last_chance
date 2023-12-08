9b60fd63e01cfbb99fb650769ae2e1d1a02f8b51
Camila Mendez <mcmendezmanotas@gmail.com>
2023-12-07 22:52:44 -0500

    adicionando el formulario de login

---
+++ src/components/loginForm/LoginForm.styled.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.div`
+  max-width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+
+export const Title = styled.h2`
+  font-family: Verdana;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  text-align: left;
+  margin 150px;
+`;
+
+export const SubTitle = styled.h3`
+  font-family: Verdana;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  text-align: left;
+`;
+
+export const FormGroup = styled.div`
+  margin-bottom: 15px;
+`;
+
+export const Label = styled.label`
+  font-family: Verdana;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  text-align: left;
+`;
+
+export const Input = styled.input`
+  width: 200px;
+  padding: 5px;
+  font-size: 1em;
+  border: none;
+  border-bottom: 1px solid #e0e0e0;
+  outline: none;
+`;
