8deb47d0a86d7a8c2838af387a009f643d598932
MariaBetancur <mabo6241@gmail.com>
2023-12-09 11:02:25 -0500

    v1

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,4 +1,14 @@
+import React from 'react';
+import { useFormik } from 'formik';
+import { useTranslation } from 'react-i18next';
 import {
+  Wrapper,
+  FormTitle,
+  FormFlexContainer,
+  BtnWrapp,
+  AuthForm,
+  Button,
+  ButtonLink,
   FormContainer,
   Title,
   SubTitle,
@@ -6,26 +16,58 @@ import {
   Label,
   Input,
 } from './RegistrationForm.styled';
+
 const RegistrationForm = () => {
+  const { t } = useTranslation();
+
+  // Formik setup
+  const formik = useFormik({});
+
+  const btnRegister = <Button type="submit">{t('auth.register')}</Button>;
+
+  const btnLogin = <ButtonLink to="/login">{t('auth.login')}</ButtonLink>;
+
+  const btnWrapp = (
+    <BtnWrapp>
+      {btnRegister}
+      {btnLogin}
+    </BtnWrapp>
+  );
+
   return (
     <FormContainer>
-      <Title>CREAR UNA CUENTA</Title>
-      <SubTitle>CREAR UNA CUENTA</SubTitle>
+      <Wrapper>
+        <FormTitle>{t('auth.registerTitle')}</FormTitle>
+        <AuthForm
+          noValidate
+          component="form"
+          autoComplete="on"
+          onSubmit={formik.handleSubmit}
+        >
+          <FormFlexContainer>
+            <FormContainer>
+              <Title>CREAR UNA CUENTA</Title>
+              <SubTitle>CREAR UNA CUENTA</SubTitle>
 
-      <FormGroup>
-        <Label htmlFor="nombre">Nombre *</Label>
-        <Input type="text" id="nombre" name="nombre" />
-      </FormGroup>
+              <FormGroup>
+                <Label htmlFor="nombre">Nombre *</Label>
+                <Input type="text" id="nombre" name="nombre" />
+              </FormGroup>
 
-      <FormGroup>
-        <Label htmlFor="correo">Correo Electrónico *</Label>
-        <Input type="email" id="correo" name="correo" />
-      </FormGroup>
+              <FormGroup>
+                <Label htmlFor="correo">Correo Electrónico *</Label>
+                <Input type="email" id="correo" name="correo" />
+              </FormGroup>
 
-      <FormGroup>
-        <Label htmlFor="contrasena">Contraseña *</Label>
-        <Input type="password" id="contrasena" name="contrasena" />
-      </FormGroup>
+              <FormGroup>
+                <Label htmlFor="contrasena">Contraseña *</Label>
+                <Input type="password" id="contrasena" name="contrasena" />
+              </FormGroup>
+            </FormContainer>
+          </FormFlexContainer>
+          {btnWrapp}
+        </AuthForm>
+      </Wrapper>
     </FormContainer>
   );
 };
--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,71 +1,68 @@
-import Calculator from "components/pages/Calculator/Calculator";
-import Diary from "components/pages/Diary/Diary";
-import Home from "components/pages/Home/Home";
-import Login from "components/pages/Login/Login";
-import SignUp from "components/pages/SignUp/SignUp";
-import { useEffect } from "react";
+// import Calculator from "components/pages/Calculator/Calculator";
+import Diary from 'components/pages/Diary/Diary';
+import Home from 'components/pages/Home/Home';
+import Login from 'components/pages/Login/Login';
+import SignUp from 'components/pages/SignUp/SignUp';
+import { useEffect } from 'react';
 
 const AppRoutes = [
-    {
-        title: "Home",
-        name: "home",
-        path: "/",
-        element: () => <Home />,
-        checkAuth: false,
-        redirectRoute: '/diary',
-    },
-    {
-        title: "Diary",
-        name: "diary",
-        path: "/diary",
-        element: () => <Diary />,
-        checkAuth: true,
-        redirectRoute: "/login",
-    },
-    {
-        title: "Calculator",
-        name: "calculator",
-        path: "/calculator",
-        element: () => <Calculator />,
-        checkAuth: true,
-        redirectRoute: "/login",
-    },
-    {
-        title: "Log In",
-        name: "logIn",
-        path: "/login",
-        element: () => <Login />,
-        checkAuth: false,
-        redirectRoute: '/diary',
-    },
-    {
-        title: "Sign Up",
-        name: "signUp",
-        path: "/register",
-        element: () => <SignUp />,
-        checkAuth: false,
-        redirectRoute: '/diary',
-    },  
-    {
-        title: "Login",
-        name: "all",
-        path: "*",
-        element: () => <Login />,
-    },
+  {
+    title: 'Home',
+    name: 'home',
+    path: '/',
+    element: () => <Home />,
+    checkAuth: false,
+    redirectRoute: '/diary',
+  },
+  {
+    title: 'Diary',
+    name: 'diary',
+    path: '/diary',
+    element: () => <Diary />,
+    checkAuth: true,
+    redirectRoute: '/login',
+  },
+  //   {
+  //     title: 'Calculator',
+  //     name: 'calculator',
+  //     path: '/calculator',
+  //     element: () => <Calculator />,
+  //     checkAuth: true,
+  //     redirectRoute: '/login',
+  //   },
+  {
+    title: 'Log In',
+    name: 'logIn',
+    path: '/login',
+    element: () => <Login />,
+    checkAuth: false,
+    redirectRoute: '/diary',
+  },
+  {
+    title: 'Sign Up',
+    name: 'signUp',
+    path: '/register',
+    element: () => <SignUp />,
+    checkAuth: false,
+    redirectRoute: '/diary',
+  },
+  {
+    title: 'Login',
+    name: 'all',
+    path: '*',
+    element: () => <Login />,
+  },
 ];
 
-
 const AppRoutesContainer_ = ({ children }) => {
-
-    useEffect(() => {
-        window.myApp = {
-            state: {
-                isLoaded: true,
-            },
-        };
-    }, []);
-    return <>{children}</>;
+  useEffect(() => {
+    window.myApp = {
+      state: {
+        isLoaded: true,
+      },
+    };
+  }, []);
+  return <>{children}</>;
 };
-  export const AppRoutesContainer = AppRoutesContainer_;
-  export default AppRoutes;
-  
+export const AppRoutesContainer = AppRoutesContainer_;
+export default AppRoutes;
