ae59769bc5bbc9b45c165c9a3cda548e77cb6fd0
Jacksondg712 <jdeviagomez@gmail.com>
2023-12-08 21:43:09 -0500

    changes 3

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,12 +1,5 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import "./Login.css"
-
-
-
-
-
-
 
 const Login = () => {
 
--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import Login from './Login';
 
 function Login() {
