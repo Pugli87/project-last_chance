c6327d8c63dc59c4cd5b099898eb08c1a3f0870a
Byron-Poveda <119381029+Byron-Poveda@users.noreply.github.com>
2023-12-09 13:54:32 -0500

    fixes routes

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import DataForm from '../../dataForm/dataForm';
-import ModalHome from 'components/Modal/ModalContent/ModalContent';
+import DataForm from '../../DataForm/dataForm';
+import ModalHome from '../../Modal/ModalContent/ModalContent';
 
 function Home() {
   return (
