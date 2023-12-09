ff48ac0933048aaf3301c8f78f3f4143b773a46c
Sr-Drachen <116126364+Sr-Drachen@users.noreply.github.com>
2023-12-09 14:20:18 -0500

    Calendario
    El calendario funcional, donde se puede cambiar la fecha y el icono del calendario. aun falta...

--- src/components/pages/Diary/Diary.jsx
+++ src/components/pages/Diary/Diary.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import GramosComponent from '../../GramosComponent/GramosComponent';
 import ButtonComponent from '../../ButtonComponent/ButtonComponent';
 import ProductNameComponent from '../../ProductNameComponent/ProductName';
@@ -6,23 +6,35 @@ import KaloriesComponent from '../../KaloriesComponent/KaloriesComponent';
 import ListnotFood from 'components/ListnotFood/ListnotFood';
 import { Container } from './DiaryStyled';
 import DateComponnet from 'components/DateComponent/DateComponent';
-
-
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ReactComponent as YourSvg } from '../../../assets/icons/calendar.svg'; // Importa tu SVG
 
 const Diary = () => {
+  const [date, setDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+
+  const handleDateChange = (selectedDate) => {
+    setDate(selectedDate);
+    setShowCalendar(false); // Oculta el calendario después de seleccionar una fecha
+  }
+
   return (
-    <Container >
-    <h1>13.08.2023</h1>
-    <ProductNameComponent />
-    <GramosComponent />
-    <ButtonComponent />
-    <KaloriesComponent />
-    <ListnotFood/>
-    <DateComponnet/>
-  </Container>
+    <Container>
+      <button onClick={() => setShowCalendar(!showCalendar)}>
+        <YourSvg />
+      </button>
+      {showCalendar && (
+        <DatePicker selected={date} onChange={handleDateChange} inline />
+      )}
+      <h1>{date.toLocaleDateString()}</h1>
+      <ProductNameComponent />
+      <GramosComponent />
+      <KaloriesComponent />
+      <ListnotFood/>
+      <DateComponnet/>
+    </Container>
   )
 }
 
-export default Diary
-
-
+export default Diary;
