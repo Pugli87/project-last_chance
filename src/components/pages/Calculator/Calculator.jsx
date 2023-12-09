369bd6c5aa92c956e6f7ac6f8c21b5c926746c00
Byron-Poveda <119381029+Byron-Poveda@users.noreply.github.com>
2023-12-09 17:54:48 -0500

    Create Component Loader

--- src/components/pages/Calculator/Calculator.jsx
+++ src/components/pages/Calculator/Calculator.jsx
@@ -1,6 +1,7 @@
 import { Container } from './calculator.styled';
 import DataForm from '../../DataForm/DataForm';
 import { Button } from 'components/Button/Button';
+import Loader from '../../Loader/Loader';
 
 const Calculator = () => {
   return (
