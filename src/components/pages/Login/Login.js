4e88b1fc34d3255a4da005a92f10e8a2054d3f8f
Jacksondg712 <jdeviagomez@gmail.com>
2023-12-10 12:27:19 -0500

    changes 4

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import "./Login.css"
+import "./Login.css";
+import { Link} from 'react-router-dom'
 
 const Login = () => {
 
@@ -16,6 +17,7 @@ const Login = () => {
     <div className='Container'>
       <h3 className='tittle'>Iniciar sesión</h3>
       <form onSubmit={handleSubmit}>
+        <div className='contCorreo'>
         <h3 className='subTittle'>Correo electrónico *</h3>
         <input 
           className='Input'
@@ -24,6 +26,8 @@ const Login = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
+        </div>
+        <div className='contContra'>
         <h3 className='subTittle'>Contraseña*</h3>
         <input
           className='Input'
@@ -32,9 +36,21 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        </div>
         <div className='ContButton'>
-        <button className='Button ButtonIniciar' type="submit"><p className='textButton textButtonIniciar'>INICIAR SESION</p></button>
-        <button className='Button ButtonCrear' type="button"><p className='textButton textButtonCrear'>CREAR UNA CUENTA</p></button>
+        <button className='Button ButtonIniciar' type="submit">
+          <p className='textButton textButtonIniciar'>INICIAR</p>
+          <p className='textButton textButtonIniciar'>SESION</p>
+
+          </button>
+        <Link
+          to={'/register'}
+          className='Button ButtonCrear'
+        >
+          <p className='textButton textButtonCrear'>CREAR UNA</p>
+          <p className='textButton textButtonCrear'>CUENTA</p>
+
+        </Link>
         </div>
       </form>
     </div>
