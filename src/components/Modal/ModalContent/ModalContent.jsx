2e7ff98165e7a4ce3d4c802333da9f2429a07f26
MariaBetancur <mabo6241@gmail.com>
2023-12-10 01:49:21 -0500

    v1.3

--- src/components/Modal/ModalContent/ModalContent.jsx
+++ src/components/Modal/ModalContent/ModalContent.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Modal from '../Modal';
 import { Button } from 'components/Button/Button';
-import BoxButton from '../../DataForm/DataForm';
+import { BoxButton } from '../../DataForm/dataForm.styled';
 import { Container } from '../Modal.styled';
 
 const ModalHome = () => {
