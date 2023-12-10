51f6ead495bfb7d1d828045b454b042c8e812305
Sr-Drachen <116126364+Sr-Drachen@users.noreply.github.com>
2023-12-10 13:57:19 -0500

    Cosas
    Estilos de calendario, añadido form y forma de eliminar elementos. falta estilos de form y llamar alimentos de base de datos u/o API

--- src/components/pages/Diary/Diary.jsx
+++ src/components/pages/Diary/Diary.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ListnotFood from 'components/ListnotFood/ListnotFood';
-import { Container, Button, Div } from './DiaryStyled';
+import { Container } from './DiaryStyled';
+import './DiaryStyled.scss'; 
 import DateComponnet from 'components/DateComponent/DateComponent';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -9,41 +10,81 @@ import { ReactComponent as YourSvg } from '../../../assets/icons/calendar.svg';
 const Diary = () => {
   const [date, setDate] = useState(new Date());
   const [showCalendar, setShowCalendar] = useState(false);
+  const [items, setItems] = useState([]);
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   const handleDateChange = selectedDate => {
     setDate(selectedDate);
     setShowCalendar(false);
   };
+
+  const handleCalendarClick = event => {
+    event.stopPropagation();
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setItems([...items, { name, number }]);
+    setName('');
+    setNumber('');
+  };
+
+  const handleDelete = (index) => {
+    setItems(items.filter((item, i) => i !== index));
+  };
+
   return (
     <Container>
       <div className="containerDiary">
-        {showCalendar && (
-          <DatePicker selected={date} onChange={handleDateChange} inline />
-        )}
         <h1>{date.toLocaleDateString()}</h1>
         <button
           className="btnDiary"
           onClick={() => setShowCalendar(!showCalendar)}
         >
+        {showCalendar && (
+          <div onClick={handleCalendarClick}>
+            <DatePicker 
+              selected={date} 
+              onChange={handleDateChange} 
+              inline 
+              maxDate={new Date()} 
+              className="customDatePicker" 
+            />
+          </div>
+        )}
           <YourSvg />
         </button>
-      </div>
-        {showCalendar && (
-          <DatePicker 
-            selected={date} 
-            onChange={handleDateChange} 
-            inline 
-            maxDate={new Date()} 
-            className="customDatePicker" 
-          />
-        )}
-        <ProductNameComponent />
-        <GramosComponent />
-        <KaloriesComponent />
-        <ListnotFood/>
-        <DateComponnet/>
+        </div>
+        <ListnotFood />
+        <DateComponnet />
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              Nombre del producto:
+              <input type="text" value={name} onChange={e => setName(e.target.value)} required />
+            </label>
+          </div>
+          <div>
+            <label>
+              Número:
+              <input type="number" value={number} onChange={e => setNumber(e.target.value)} required />
+            </label>
+          </div>
+          <div>
+            <button n type="submit">Guardar</button>
+          </div>
+        </form>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.name} - {item.number}
+              <button onClick={() => handleDelete(index)}>Eliminar</button>
+            </li>
+          ))}
+        </ul>
     </Container>
-  )
-}
+  );
+};
 
-export default Diary
+export default Diary;
--- src/components/pages/Diary/DiaryStyled.js
+++ src/components/pages/Diary/DiaryStyled.js
@@ -1,6 +1,4 @@
+import styled from 'styled-components';
 export const Container = styled.div`
   margin-top: 100px;
-
 `;
-
-
