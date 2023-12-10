67bd4b07e38225dac3eab165856e3a9be77c5bd6
Camila Mendez <mcmendezmanotas@gmail.com>
2023-12-09 20:18:31 -0500

    adicionando reponsive pagina login y calculator

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DataForm from '../../DataForm/DataForm';
+import DataForm from '../../dataForm/dataForm';
 import ModalHome from '../../Modal/ModalContent/ModalContent';
 
 function Home() {
