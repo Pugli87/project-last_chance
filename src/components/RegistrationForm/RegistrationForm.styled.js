2748eb9b772a4ccf05c7013aa3580d582c66b7c8
MariaBetancur <mabo6241@gmail.com>
2023-12-09 21:13:48 -0500

    v1

--- src/components/RegistrationForm/RegistrationForm.styled.js
+++ src/components/RegistrationForm/RegistrationForm.styled.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export const FormContainer = styled.div`
   max-width: 350px;
@@ -49,4 +51,90 @@ export const Input = styled.input`
   border: none;
   border-bottom: 1px solid #e0e0e0;
   outline: none;
+`;
+export const AuthForm = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 280px;
+`;
+export const BtnWrapp = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Button = styled.button`
+  width: 182px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  cursor: pointer;
+
+  &:not(:last-child) {
+    margin-top: 40px;
+    margin-bottom: 20px;
+  }
+
+  color: ${props => props.theme.fontColors.buttonPrimary};
+  background: ${props => props.theme.backgrounds.buttonPrimary};
+  border: none;
+  box-shadow: 0px 4px 10px rgba(252, 132, 45, 0.5);
+  border-radius: 30px;
+  transition: transform ${props => props.theme.transitions.primary},
+    background-color ${props => props.theme.transitions.primary};
+
+  &:hover,
+  &:focus {
+    transform: scale(1.1);
+    background-color: ${props => props.theme.hovers.secondary};
+  }
+`;
+export const FormTitle = styled.h1`
+  font-family: 'GothamproBold';
+  font-style: normal;
+  font-size: 14px;
+  line-height: 17px;
+  text-transform: uppercase;
+  letter-spacing: 0.04em;
+  margin-bottom: 60px;
+  color: ${props => props.theme.fontColors.modify};
+`;
+export const FormFlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+`;
+export const ButtonLink = styled(Link)`
+  width: 182px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 17px;
+
+  letter-spacing: 0.04em;
+  cursor: pointer;
+
+  color: ${props => props.theme.fontColors.buttonSecondary};
+  background: ${props => props.theme.backgrounds.buttonSecondary};
+  border: 2px solid ${props => props.theme.backgrounds.buttonPrimary};
+  border-radius: 30px;
+  transition: transform ${props => props.theme.transitions.primary};
+
+  &:hover,
+  &:focus {
+    transform: scale(1.1);
+  }
 `;
