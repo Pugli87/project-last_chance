1ccfa65ccff3505c073f6805f834093fcbfbdef2
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-12-11 17:57:29 -0500

    coreecion

--- src/components/DateComponent/DateComponent.jsx
+++ src/components/DateComponent/DateComponent.jsx
@@ -1,42 +1,12 @@
 import React from 'react';
 import { Container, Title, SubtitleDate, CaloricValue } from './DateStyled';
-/*
+
 const DateComponnet = () => {
   return (
     <Container>
       <Title>Resumen para el 13.08.2023</Title>
       <ul>
         <li>
-          <h3></h3>
-          <p id="parragraph"></p>
-        </li>
-        <li>
-          <h3></h3>
-          <p id="parragraph"></p>
-        </li>
-        <li>
-          <h3></h3>
-          <p id="parragraph"></p>
-        </li>
-        <li>
-          <h3></h3>
-          <p id="parragraph"></p>
-        </li>
-      </ul>
-      <SubtitleDate>Quedan</SubtitleDate> <CaloricValue>625 kcal</CaloricValue>
-      <SubtitleDate>Cosumido</SubtitleDate> <CaloricValue>2175 kcal</CaloricValue>
-      <SubtitleDate>Tasa diaria</SubtitleDate> <CaloricValue>2800 kcal</CaloricValue>
-      <SubtitleDate>n% de lo normal</SubtitleDate> <CaloricValue>78%</CaloricValue>
-    </Container>
-  );
-};*/
-const DateComponnet = () => {
-  return (
-    <Container>
-      <Title>Resumen para el 13.08.2023</Title>
-      <ul>
-        <li>
-          {' '}
           {/*display: flex; justify-content: space-between; */}
           <h3>Quedan</h3>
           <p id="parragraph">625 kcal</p>
