401d6c4aaf955d11dea7b7f3f4d48c5d58657432
camilorivera84 <110140389+camilorivera84@users.noreply.github.com>
2023-12-11 19:34:19 -0500

    logout_ok

--- src/components/Header/HeaderNavbar/HeaderNavbar.jsx
+++ src/components/Header/HeaderNavbar/HeaderNavbar.jsx
@@ -6,13 +6,13 @@ import NavLinkHeader from '../NavLinkHeader/NavLinkHeader';
 import { useDeviceDetect } from '../../../hooks/deviceDetect/useDeviceDetect';
 import HeaderMenuMobile from '../HeaderMenuMobile/HeaderMenuMobile';
 import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch /*, useSelector*/ } from 'react-redux';
 import { logOutUser } from '../../../redux/thunks';
 import Modal from '../../Modal/ModalCalculator';
 
 function HeaderNavbar() {
   const { isDesktop, isMobile } = useDeviceDetect();
-  const token = useSelector(state => state.auth.token);
+  //const token = useSelector(state => state.auth.token);
   const dispatch = useDispatch();
 
   const [showModal, setShowModal] = useState(false);
