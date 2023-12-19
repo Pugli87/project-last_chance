45844b5f4b43bc3c569771e319636b0b27986cbb
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-12-19 00:03:01 -0500

    correccion stylos boton crear

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Login.css';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../../redux/thunks';
 import {
