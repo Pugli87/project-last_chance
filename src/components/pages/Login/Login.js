88df9053f4731173cb4606b5d848eae0938ff7e9
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-12-18 22:30:47 -0500

    delete login.css

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import "./Login.css";
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../../redux/thunks';
 import {
@@ -14,7 +13,7 @@ import {
   ContFormContra,
   Button,
   TextButtonIniciar,
-  TextButtonCrear
+  TextButtonCrear,
 } from './Login.styled';
 
 const Login = () => {
@@ -25,79 +24,75 @@ const Login = () => {
   const [emailValid, setEmailValid] = useState(true);
   const [passwordValid, setPasswordValid] = useState(true);
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(loginUser({ email, password }));
 
+    if (!email.trim()) {
+      setEmailValid(false);
+    } else {
+      setEmailValid(true);
+    }
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      dispatch(loginUser({email, password}))
+    if (!password.trim()) {
+      setPasswordValid(false);
+    } else {
+      setPasswordValid(true);
+    }
 
-      if (!email.trim()) {
-        setEmailValid(false);
-      } else {
-        setEmailValid(true);
-      }
-  
-  
-      if (!password.trim()) {
-        setPasswordValid(false);
-      } else {
-        setPasswordValid(true);
-        
-      }
-  
-      if (emailValid && passwordValid) {
-        console.log('Correo:', email);
-        console.log('Contraseña:', password);
-      }
+    if (emailValid && passwordValid) {
+      console.log('Correo:', email);
+      console.log('Contraseña:', password);
+    }
   };
-  
-
-
 
   return (
     <Container>
       <Tittle>Iniciar sesión</Tittle>
       <form onSubmit={handleSubmit}>
         <ContFormCorreo>
-          <SubTittle className='subTittle'>Correo Electronico*</SubTittle>
-          {emailValid ? <Input 
-            type="email"
-            id="email"
-            value={email}
-            onChange= {(e) => setEmail(e.target.value)}
-            /> : <InputWanning
-            type="email"
-            id="email"
-            placeholder="El correo electronico es un campo obligatorio"
-            value={email}
-            onChange= {(e) => setEmail(e.target.value)}
-          />}
-          </ContFormCorreo>
-          <ContFormContra >
-          <SubTittle className='subTittle'>Contraseña*</SubTittle>
-          {passwordValid ? <Input 
-            type="password"
-            placeholder=""
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            /> : <InputWanning
-            type="password"
-            placeholder="La contraseña es un campo obligatorio"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+          <SubTittle className="subTittle">Correo Electronico*</SubTittle>
+          {emailValid ? (
+            <Input
+              type="email"
+              id="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
-          }
-  
-          </ContFormContra>
+          ) : (
+            <InputWanning
+              type="email"
+              id="email"
+              placeholder="El correo electronico es un campo obligatorio"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          )}
+        </ContFormCorreo>
+        <ContFormContra>
+          <SubTittle className="subTittle">Contraseña*</SubTittle>
+          {passwordValid ? (
+            <Input
+              type="password"
+              placeholder=""
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          ) : (
+            <InputWanning
+              type="password"
+              placeholder="La contraseña es un campo obligatorio"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          )}
+        </ContFormContra>
         <ContButton>
           <Button type="submit">
             <TextButtonIniciar>INICIAR</TextButtonIniciar>
             <TextButtonIniciar>SESION</TextButtonIniciar>
           </Button>
-          <Link
-            to={'/register'}
-            className='Button ButtonCrear'
-          >
+          <Link to={'/register'} className="Button ButtonCrear">
             <TextButtonCrear>CREAR UNA</TextButtonCrear>
             <TextButtonCrear>CUENTA</TextButtonCrear>
           </Link>
